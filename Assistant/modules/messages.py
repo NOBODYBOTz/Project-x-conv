42571a9162c9e34ec254352e1d8b9b6773882387
NOBODY BOTz <140589004+NOBODYBOTz@users.noreply.github.com>
2024-11-05 13:15:45 +0530

    Update messages.py

--- Assistant/modules/messages.py
+++ Assistant/modules/messages.py
@@ -14,13 +14,35 @@ async def incoming_groups(update: Update, context: ContextTypes.DEFAULT_TYPE) ->
     message = cast(Message, update.effective_message)
     if not await is_group():
         return None
-
     if reply := message.reply_to_message:
-        user = None
-        if reply.forward_origin and reply.forward_origin.type == "user":
+        if reply.forward_origin.type == "user":
             user = reply.forward_origin.sender_user
+            if not user:
+                return None
 
-        if user:
+        try:
+            return await bot.copy_message(
+                user.id,
+                message.chat.id,
+                message.id,
+            )
+        except Exception as e:
+            LOGGER.warning(str(e))
+            return await message.reply_text(f"ғᴀɪʟᴇᴅ ᴛᴏ sᴇɴᴅ ᴛʜᴇ ᴍᴇssᴀɢᴇ\n\nᴇʀʀᴏʀ:{e} ")
+
+
+async def incoming_private(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    bot = context.bot
+    message = cast(Message, update.effective_message)
+    if await is_banned_user(message.from_user.id):
+        return None
+    if message.from_user.id in SUDO_USERS:
+        if reply := message.reply_to_message:
+            if reply.forward_origin.type == "user":
+                user = reply.forward_origin.sender_user
+                if not user:
+                    return None
+
             try:
                 return await bot.copy_message(
                     user.id,
@@ -29,37 +51,9 @@ async def incoming_groups(update: Update, context: ContextTypes.DEFAULT_TYPE) ->
                 )
             except Exception as e:
                 LOGGER.warning(str(e))
-                return await message.reply_text(f"ғᴀɪʟᴇᴅ ᴛᴏ sᴇɴᴅ ᴛʜᴇ ᴍᴇssᴀɢᴇ\n\nᴇʀʀᴏʀ:{e} ")
-        else:
-            return await message.reply_text("User not found.")
-
-
-async def incoming_private(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    bot = context.bot
-    message = cast(Message, update.effective_message)
-    if await is_banned_user(message.from_user.id):
-        return None
-
-    if message.from_user.id in SUDO_USERS:
-        if reply := message.reply_to_message:
-            user = None
-            if reply.forward_origin and reply.forward_origin.type == "user":
-                user = reply.forward_origin.sender_user
-
-            if user:
-                try:
-                    return await bot.copy_message(
-                        user.id,
-                        message.chat.id,
-                        message.id,
-                    )
-                except Exception as e:
-                    LOGGER.warning(str(e))
-                    return await message.reply_text(
-                        f"ғᴀɪʟᴇᴅ ᴛᴏ sᴇɴᴅ ᴛʜᴇ ᴍᴇssᴀɢᴇ\n\nᴇʀʀᴏʀ:{e} "
-                    )
-            else:
-                return await message.reply_text("User not found.")
+                return await message.reply_text(
+                    f"ғᴀɪʟᴇᴅ ᴛᴏ sᴇɴᴅ ᴛʜᴇ ᴍᴇssᴀɢᴇ\n\nᴇʀʀᴏʀ:{e} "
+                )
     elif await is_group():
         await bot.forward_messages(
             chat_id=config.LOGGER_ID,
